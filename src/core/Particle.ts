11738adbaf61c5189619f0a8e28646acff5c4364
ferz1ka <fhozanzoti@hotmail.com>
2026-04-02 23:20:23 -0300

    feat: add triangle shape

--- src/core/Particle.ts
+++ src/core/Particle.ts
@@ -1,4 +1,4 @@
-import { config } from './Config';
+import { config } from "./Config";
 
 export class Particle {
   x: number;
@@ -12,10 +12,16 @@ export class Particle {
   rotation: number;
   rotationSpeed: number;
 
-  constructor(x: number, y: number, color: string, mouseVx: number = 0, mouseVy: number = 0) {
+  constructor(
+    x: number,
+    y: number,
+    color: string,
+    mouseVx: number = 0,
+    mouseVy: number = 0,
+  ) {
     this.x = x;
     this.y = y;
-    
+
     // Spread based on config speed and spread
     const angle = Math.random() * Math.PI * 2;
     const r = Math.random() * config.spread;
@@ -26,11 +32,13 @@ export class Particle {
       this.vx += mouseVx * config.inheritFactor;
       this.vy += mouseVy * config.inheritFactor;
     }
-    
+
     this.life = 1.0;
     this.color = color;
-    
-    this.size = config.randomizeSize ? Math.random() * config.size + config.size / 2 : config.size;
+
+    this.size = config.randomizeSize
+      ? Math.random() * config.size + config.size / 2
+      : config.size;
     this.shape = config.shape;
     this.rotation = Math.random() * Math.PI * 2;
     this.rotationSpeed = (Math.random() - 0.5) * 0.2;
@@ -39,10 +47,10 @@ export class Particle {
   update() {
     this.x += this.vx;
     this.y += this.vy;
-    
+
     this.vx *= config.deceleration;
     this.vy *= config.deceleration;
-    
+
     this.life -= config.delayFade;
     this.rotation += this.rotationSpeed;
   }
@@ -59,20 +67,43 @@ export class Particle {
     ctx.shadowBlur = 10;
     ctx.shadowColor = this.color;
 
-    if (this.shape === 'Circle') {
+    if (this.shape === "Circle") {
       ctx.beginPath();
       ctx.arc(0, 0, this.size, 0, Math.PI * 2);
       ctx.fill();
-    } else if (this.shape === 'Square') {
+    } else if (this.shape === "Square") {
       ctx.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
-    } else if (this.shape === 'Star') {
+    } else if (this.shape === "Star") {
       this.drawStar(ctx, 0, 0, 5, this.size, this.size / 2);
+    } else if (this.shape === "Triangle") {
+      this.drawTriangle(ctx, 0, 0, this.size);
     }
 
     ctx.restore();
   }
 
-  drawStar(ctx: CanvasRenderingContext2D, cx: number, cy: number, spikes: number, outerRadius: number, innerRadius: number) {
+  drawTriangle(
+    ctx: CanvasRenderingContext2D,
+    cx: number,
+    cy: number,
+    size: number,
+  ) {
+    ctx.beginPath();
+    ctx.moveTo(cx, cy - size);
+    ctx.lineTo(cx + size, cy + size);
+    ctx.lineTo(cx - size, cy + size);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  drawStar(
+    ctx: CanvasRenderingContext2D,
+    cx: number,
+    cy: number,
+    spikes: number,
+    outerRadius: number,
+    innerRadius: number,
+  ) {
     let rot = (Math.PI / 2) * 3;
     let x = cx;
     let y = cy;
